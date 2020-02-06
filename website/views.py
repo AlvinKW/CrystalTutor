42d241cd12c5b088f0efcb9327865a05def09d55
Alvin Kwan <alvink88@bu.edu>
2020-02-05 21:48:16 -0500

    Test

---
+++ website/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+def home(request):
+	if request.method == "POST":
+		#do stuff
+		your_name = request.POST['your-name']
+		your_phone = request.POST['your-phone']
+		your_email = request.POST['your-email']
+		your_address = request.POST['your-address']
+		your_time = request.POST['your-time']
+		your_message = request.POST['your-message']
+		
+		#send email
+		send_mail(
+			'Crystal Tutor Message From: ' + your_name, #subject
+			'Name: ' + your_name + '\nPhone :' + your_phone + '\nAddress: ' + your_address + '\n Email: ' + your_email + '\nTime slot: ' + your_time + '\n\nMessage: ' + your_message, #message
+			your_email, #from email
+			['kwanalvin150@gmail.com'], #to email
+			fail_silently=False,
+			)
+
+		return render(request, 'home.html', {'your_message':your_message})
+
+	else:
+
+		return render(request, 'home.html', {})
