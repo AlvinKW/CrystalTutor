6506fe18f7d6c0d571b7656250741fe8345db45c
Alvin Kwan <alvink88@bu.edu>
2020-02-06 00:21:07 -0500

    tweak email

--- tutor/settings.py
+++ tutor/settings.py
@@ -142,10 +142,11 @@ STATICFILES_DIRS = [
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TSL = True
-EMAIL_PORT = 587
 EMAIL_HOST_USER = 'crystaltutorwebsite@gmail.com'
 EMAIL_HOST_PASSWORD = 'abc123xyz'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+
 
 
 
