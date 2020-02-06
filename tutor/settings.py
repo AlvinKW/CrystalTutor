affbadc86db894674bee75ab549da7de1dda4ec1
Alvin Kwan <alvink88@bu.edu>
2020-02-06 00:41:34 -0500

    tweak

--- tutor/settings.py
+++ tutor/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3i7lu0maa3xwg$sqb=7stu&l3kq+n$)l)!ss8x9xc2&%3s@aw='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
