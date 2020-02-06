e411a885185a876a4023e7efa64c129e319836db
Alvin Kwan <alvink88@bu.edu>
2020-02-06 00:47:45 -0500

    tweaked

--- tutor/settings.py
+++ tutor/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '3i7lu0maa3xwg$sqb=7stu&l3kq+n$)l)!ss8x9xc2&%3s@aw='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['crystalchaututoring.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'crystalchaututoring.herokuapp.com']
 
 
 # Application definition
